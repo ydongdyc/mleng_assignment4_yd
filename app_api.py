8eb7412000789288220e8e501f1bb1d9492b5ae1
ydongdyc <yanchendong@hotmail.com>
2025-05-30 15:13:15 -0500

    Update app_api.py

--- app_api.py
+++ app_api.py
@@ -18,9 +18,10 @@ def get_status() -> dict:
     return {"status": "OK"}
 
 @app.get("/version")
-def get_status() -> dict:
-    """Returns the server status."""
+def get_version() -> dict:
+    """Returns the server version."""
     return {"status": "1.1"}
+
 
 @app.get("/sayhi/{name}")
 def say_hi(name: str) -> dict:

e83918b8ef98218c9c54841077234d42c7024dbb
ydongdyc <yanchendong@hotmail.com>
2025-05-30 15:10:00 -0500

    making updates to app file

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,5 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD ["python", "app.py"]
+#CMD ["fastapi", "run", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "app_api:app", "--host", "0.0.0.0", "--port", "8000"]
--- app.py
+++ app.py
@@ -2,24 +2,21 @@ import os
 from datetime import datetime
 
 # Please customize the following variables 
-USER_NAME = "shahbazc"
+USER_NAME = "ydong"
 VERSION = "1.0.0"
 
-def say_hi(msg:str = "Hi!", file_directory:str = "/app/data/") -> None:
-    # Generate timestamp
+def say_hi(msg: str = "Hi!", file_directory: str = "/app/data/") -> None:
     timestamp = datetime.now().strftime("%Y%m%d%H%M")
-
-    # Define filename with timestamp
     file_name = f"outputfile_{USER_NAME}_{VERSION}_timestamp_{timestamp}.txt"
-    file_path = os.join(file_directory, file_name)
+    file_path = os.path.join(file_directory, file_name)
 
-    # Write the timestamp inside the file
+
     with open(file_path, "w") as file:
         file.write(msg)
 
     print(f"File '{file_path}' created successfully.")
 
-def add_numbers(a:int, b:int) -> int:
+def add_numbers(a: int, b: int) -> int:
     return a + b
 
 if __name__ == "__main__":
